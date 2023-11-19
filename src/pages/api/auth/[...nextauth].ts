5b1ac09d80f08ad168146c79b31bfa81077f7692
DannyS123 <szdani2012@gmail.com>
2023-11-19 15:57:18 +0100

    authSch fix

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -3,6 +3,23 @@ import prisma from '@/lib/prisma'
 import { PrismaAdapter } from '@next-auth/prisma-adapter'
 import NextAuth, { NextAuthOptions } from 'next-auth'
 
+type Entitlement = {
+  id: number
+  name: string
+  title: string[]
+  status: string
+  start: Date
+  end: string | null
+}
+
+type AuthSchProfile = {
+  internal_id: string
+  displayName: string
+  givenName: string
+  mail: string
+  eduPersonEntitlement: Entitlement[]
+}
+
 export const authOptions: NextAuthOptions = {
   // Configure one or more authentication providers
   adapter: PrismaAdapter(prisma),
@@ -29,16 +46,16 @@ export const authOptions: NextAuthOptions = {
       },
       clientId: process.env.AUTHSCH_CLIENT_ID,
       clientSecret: process.env.AUTHSCH_CLIENT_SECRET,
-      profile(profile) {
+      profile(profile: AuthSchProfile) {
         //TODO
         //const impulzusTitles = profile.eduPersonEntitlement.map((e: any) => e.name === 'Impulzus')[0].title
         return {
-          id: '1',
+          id: profile.internal_id,
           authSchId: profile.internal_id,
           email: profile.mail,
           name: profile.displayName,
-          emailVerified: profile.emailVerified,
-          titles: profile.eduPersonEntitlement[0].title,
+          emailVerified: true,
+          titles: profile?.eduPersonEntitlement?.[0]?.title ?? [],
           isAdmin: false,
           isBoardMember: false,
           picture: ''
@@ -47,72 +64,13 @@ export const authOptions: NextAuthOptions = {
     }
   ],
   secret: process.env.NEXTAUTH_SECRET,
-  /*events: {
-    async signIn(message) {
-      //console.log('Message: ' + message)
-      // on successful sign in
-    }
-  },
- logger: {
-    error(code, metadata) {
-      console.error(code, metadata)
-    },
-    warn(code) {
-      //console.warn(code)
-    },
-    debug(code, metadata) {
-      //console.debug(code, metadata)
-    }
-  },*/
   debug: true,
-
   callbacks: {
     async session({ session, user }) {
-      // Send properties to the client, like an access_token from a provider.
       session.user = user
-
-      const userDetails = await prisma.user.findUnique({
-        where: {
-          id: user.id
-        }
-      })
-
-      session.user = userDetails || undefined
-
       return session
     }
   }
-
-  /*callbacks: {
-    async jwt({ token, user }) {
-      // Step 1: update the token based on the user object
-      if (user) {
-        token.isAdmin = user.isAdmin
-        token.isBoardMember = user.isBoardMember
-        console.log(token)
-      }
-      return token
-    },
-    async session({ session, user, token }) {
-      // Step 2: update the session.user based on the token object
-      if (token && session.user) {
-        session.user.isAdmin = token.isAdmin
-        session.user.isBoardMember = token.isBoardMember
-      }
-      return session
-    }
-  }*/
-
-  /*session: {
-    strategy: 'jwt'
-  }*/
-
-  /*theme: {
-    colorScheme: 'auto', // "auto" | "dark" | "light"
-    brandColor: '', // Hex color code
-    logo: '', // Absolute URL to image
-    buttonText: '' // Hex color code
-  }*/
 }
 
 export default NextAuth(authOptions)
